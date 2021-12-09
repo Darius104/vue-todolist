26e0e8417008694c76715e9b313ab037064171a5
Darius104 <comandarius@gmail.com>
2021-12-09 17:38:50 +0100

    MILESTONE 1 DONE

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -1,3 +1,38 @@
 // MILESTONE 1
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        listToDo: [
+            {
+                testo: 'Lava la macchina',
+                done: false
+            },
+            {
+                testo: 'Fare la spesa',
+                done: true
+            },
+            {
+                testo: 'Comprare il pane',
+                done: true
+            },
+            {
+                testo: 'Andare in palestra',
+                done: true
+            },
+            {
+                testo: 'Ripassare',
+                done: true
+            }
+        ]
+    },
+    methods: {
+        addText: function(){
+            
+        }
+    }
+});
